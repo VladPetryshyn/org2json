e87136f43c1cb861ee34d901502f6f77bd9881b2
VladPetryshyn <vlad_petryshyn@pm.me>
2022-11-23 17:17:14 +0200

    fixed tags generation

--- index.ts
+++ index.ts
@@ -109,7 +109,7 @@ export const orgToJson = (f: string): { notes: Notes; tags: Tags } => {
       } else if (new RegExp(/\:/).test(item[i])) {
         //tag
         const [tag, ni] = extractTag(i + 1, item);
-        if (tag) {
+        if (tag && level >= 1) {
           i = ni;
           note.tags.push(tag);
           tags[tag] = tags[tag] ? ++tags[tag] : 1
